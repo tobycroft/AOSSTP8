103e7a2d2a7a79123acff8f89494232c25b07a6c
火线兔 <silverhawk@foxmail.com>
2023-07-27 21:19:52 +0800

    TuuzPC

--- app/v1/file/controller/index.php
+++ app/v1/file/controller/index.php
@@ -57,7 +57,7 @@ class index extends search
         if (!$file) {
             Ret::Fail(400, null, 'file字段没有用文件提交');
         }
-        $file_name = $file->getFilename('name');
+        $file_name = $file->getFilename();
         $md5 = $file->hash('md5');
         $sha1 = $file->hash("sha1");
         $mime = $file->getFileInfo('type');
