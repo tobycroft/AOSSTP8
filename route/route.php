533c2b28a4f25718d2542cf0a35a2271dcfa0e0a
火线兔 <silverhawk@foxmail.com>
2023-07-27 23:09:15 +0800

    TuuzPC

--- route/route.php
+++ route/route.php
@@ -55,6 +55,6 @@
     if (\think\facade\Request::isOptions()) {
         return false;
     }
-    return 'AOSS is a private platform! </br> Contact oss@tuuz.cc with your reason to join us! </br> Aoss is now support GPT-APIs';
+    return 'AOSS is a private platform! </br> Aoss is now support GPT-APIs </br> Contact oss@tuuz.cc with your reason to join us! ';
 });
 
