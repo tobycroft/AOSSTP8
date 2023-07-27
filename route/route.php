ef86566124d6413917c29fde0dd3e281953eb052
火线兔 <silverhawk@foxmail.com>
2023-07-27 22:57:42 +0800

    TuuzPC

--- route/route.php
+++ route/route.php
@@ -55,6 +55,6 @@
     if (\think\facade\Request::isOptions()) {
         return false;
     }
-    return "AOSS is a private platform! \n To use this platform plz contact oss@tuuz.cc!";
+    return 'AOSS is a private platform! </br> To use this platform plz contact oss@tuuz.cc!';
 });
 
