506cc0c3db3c8baca511f1a8e1be7ecf111fb70e
火线兔 <silverhawk@foxmail.com>
2023-07-27 22:35:15 +0800

    TuuzPC

--- app/v1/file/controller/index.php
+++ app/v1/file/controller/index.php
@@ -63,7 +63,7 @@ class index extends search
         $mime = $file->getOriginalMime();
         // 判断附件格式是否符合
 
-        $file_exists = AttachmentModel::where(['token' => $token, 'md5' => $md5, 'sha1' => $sha1])->findOrEmpty();
+        $file_exists = AttachmentModel::where(['token' => $token, 'md5' => $md5, 'sha1' => $sha1])->find();
 
         if ($file_exists) {
             if ($proc['type'] != 'all' || file_exists('./upload/' . $file_exists['path'])) {
