3f743dc1b2f7520e505df2edee6a519d46582f88
火线兔 <silverhawk@foxmail.com>
2023-07-30 21:59:58 +0800

    TuuzPC

--- app/v1/ai/controller/bing.php
+++ app/v1/ai/controller/bing.php
@@ -18,10 +18,16 @@ class bing
 
 // $text - Text-only version of Bing's answer
 // $cards - Message objects array
-        list($text, $cards) = $conversation->ask(new Prompt('Hello World'));
         $valid = $ai->checkCookie();
         var_dump($valid);
-        print_r($text);
-        print_r($cards);
+
+        $subject = 'Internet memes';
+        $tone = 'funny';
+        $type = 'blog post';
+        $length = 'short';
+
+        $prompt = new Prompt("Please write a *$length* *$type* in a *$tone* style about `$subject`. Please wrap the $type in a markdown codeblock.");
+
+        var_dump($conversation->ask($prompt->withoutCache()));
     }
 }
