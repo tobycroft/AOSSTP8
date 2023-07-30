d65d3541432373fcadb9af2557dba6f78f1ab880
火线兔 <silverhawk@foxmail.com>
2023-07-30 21:54:08 +0800

    TuuzPC

---
+++ app/v1/ai/controller/bing.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\v1\ai\controller;
+
+use app\v1\system\model\SystemParamModel;
+use MaximeRenou\BingAI\BingAI;
+use MaximeRenou\BingAI\Chat\Prompt;
+
+class bing
+{
+
+    public function index()
+    {
+        // $cookie - your "_U" cookie from bing.com
+        $ai = new BingAI(SystemParamModel::api_find_key('bing_cookie'));
+
+        $conversation = $ai->createChatConversation();
+
+// $text - Text-only version of Bing's answer
+// $cards - Message objects array
+        list($text, $cards) = $conversation->ask(new Prompt('Hello World'));
+    }
+}
