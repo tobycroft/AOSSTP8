7220c9eec0f3b81975e5590e35094ab38b56c5e2
火线兔 <silverhawk@foxmail.com>
2023-07-30 22:53:02 +0800

    TuuzPC

--- app/v1/ai/controller/bing.php
+++ app/v1/ai/controller/bing.php
@@ -35,5 +35,34 @@ class bing
 
         // Print final answer
         echo "- $text" . PHP_EOL;
+
+        // Generative cards
+        foreach ($cards as $card) {
+            if ($card->type == \MaximeRenou\BingAI\Chat\MessageType::GenerateQuery && $card->data['contentType'] == 'IMAGE') {
+                $loader = "Generating: {$card->text}...";
+                echo $loader;
+
+                // Create the image
+                $creator = $ai->createImages($card->text);
+                $creator->wait();
+
+                echo str_repeat(chr(8), strlen($loader));
+
+                if ($creator->hasFailed()) {
+                    echo '[Image generation failed]' . PHP_EOL;
+                } else {
+                    foreach ($creator->getImages() as $image) {
+                        echo "* $image" . PHP_EOL;
+                    }
+
+                    $remaining = $creator->getRemainingBoosts();
+                    echo "[$remaining remaining boosts]" . PHP_EOL;
+                }
+            }
+        }
+
+        if ($conversation->ended()) {
+            echo '[Conversation ended]' . PHP_EOL;
+        }
     }
 }
