caa7e81f5db76e73767d599fed7fac80287f9abc
火线兔 <silverhawk@foxmail.com>
2023-07-30 21:39:40 +0800

    TuuzPC

--- app/v1/ai/controller/bard.php
+++ app/v1/ai/controller/bard.php
@@ -12,9 +12,10 @@ class bard extends CommonController
     {
 
 //two keys are required which are two cookies values
-        SystemParamModel::where("key")
-        $_ENV['BARD_API_KEY_X'] = " value of cookie '' ";
-        $_ENV['BARD_API_KEY_Y'] = " value of cookie '' ";
+
+
+        $_ENV['BARD_API_KEY_X'] = SystemParamModel::api_find_key('__Secure-1PSID');;
+        $_ENV['BARD_API_KEY_Y'] = SystemParamModel::api_find_key("__Secure-1PSIDTS");
         $bard = new \Pj8912\PhpBardApi\Bard();
         $input_text = 'Hello, Bard!';  // Input text for the conversation
         $result = $bard->get_answer($input_text);  // Get the response from Bard
