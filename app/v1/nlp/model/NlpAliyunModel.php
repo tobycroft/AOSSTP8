ab7abf60dff758aef430bd40ed016bd937590a3e
火线兔 <silverhawk@foxmail.com>
2023-08-24 16:27:48 +0800

    TuuzMac

---
+++ app/v1/nlp/model/NlpAliyunModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\v1\nlp\model;
+
+use think\Model;
+
+class NlpAliyunModel extends Model
+{
+    protected $table = "ao_nlp_aliyun";
+}
---
+++ app/v1/nlp/model/NlpModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\v1\nlp\model;
+
+use think\Model;
+
+class NlpModel extends Model
+{
+    protected $table = "ao_nlp_aliyun";
+
+    public static function Api_find_project($project)
+    {
+
+    }
+}
+
