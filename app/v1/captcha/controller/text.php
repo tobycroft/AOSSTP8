9c552a6b09d8ed2fce9c9b5cd8242492ec56ceeb
火线兔 <silverhawk@foxmail.com>
2023-09-01 11:33:28 +0800

    TuuzMac

--- app/v1/captcha/controller/text.php
+++ app/v1/captcha/controller/text.php
@@ -12,7 +12,7 @@ class text extends create
     {
         $config = [
             //验证码位数
-            'length' => 5,
+            'length' => 4,
             // 验证码字符集合
             'codeSet' => '2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
             // 验证码过期时间
@@ -20,7 +20,7 @@ class text extends create
             // 是否使用中文验证码
             'useZh' => false,
             // 是否使用算术验证码
-            'math' => false,
+            'math' => true,
             // 是否使用背景图
             'useImgBg' => false,
             //验证码字符大小
