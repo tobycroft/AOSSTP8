8262948b5bb7721c1796241433865c9860a620f0
火线兔 <silverhawk@foxmail.com>
2023-09-01 13:22:30 +0800

    TuuzMac

--- app/v1/captcha/controller/text.php
+++ app/v1/captcha/controller/text.php
@@ -22,38 +22,19 @@ class text extends create
     {
 
         $config = [
-            //验证码位数
             'length' => 2,
-            // 验证码字符集合
             'codeSet' => '0123456789',
-            // 验证码过期时间
             'expire' => 1800,
-            // 是否使用中文验证码
             'useZh' => true,
-            // 是否使用算术验证码
             'math' => false,
-            // 是否使用背景图
             'useImgBg' => false,
-            //验证码字符大小
             'fontSize' => 25,
-            // 是否使用混淆曲线
             'useCurve' => true,
-            //是否添加杂点
             'useNoise' => true,
-            // 验证码字体 不设置则随机
             'fontttf' => '',
-            //背景颜色
             'bg' => [243, 251, 254],
-            // 验证码图片高度
             'imageH' => 0,
-            // 验证码图片宽度
             'imageW' => 0,
-
-            // 添加额外的验证码设置
-            // verify => [
-            //     'length'=>4,
-            //    ...
-            //],
         ];
         $con = new \think\Config();
         $con->set($config, 'captcha');
@@ -73,38 +54,19 @@ class text extends create
     public function number()
     {
         $config = [
-            //验证码位数
             'length' => 4,
-            // 验证码字符集合
             'codeSet' => '0123456789',
-            // 验证码过期时间
             'expire' => 1800,
-            // 是否使用中文验证码
             'useZh' => false,
-            // 是否使用算术验证码
             'math' => false,
-            // 是否使用背景图
             'useImgBg' => false,
-            //验证码字符大小
             'fontSize' => 25,
-            // 是否使用混淆曲线
             'useCurve' => false,
-            //是否添加杂点
             'useNoise' => true,
-            // 验证码字体 不设置则随机
             'fontttf' => '',
-            //背景颜色
             'bg' => [243, 251, 254],
-            // 验证码图片高度
             'imageH' => 0,
-            // 验证码图片宽度
             'imageW' => 0,
-
-            // 添加额外的验证码设置
-            // verify => [
-            //     'length'=>4,
-            //    ...
-            //],
         ];
         $con = new \think\Config();
         $con->set($config, "captcha");
@@ -124,38 +86,19 @@ class text extends create
     public function text()
     {
         $config = [
-            //验证码位数
             'length' => 4,
-            // 验证码字符集合
             'codeSet' => '0123456789QWERTYUIOPASDFGHJKLZXCVBNM',
-            // 验证码过期时间
             'expire' => 1800,
-            // 是否使用中文验证码
             'useZh' => false,
-            // 是否使用算术验证码
             'math' => false,
-            // 是否使用背景图
             'useImgBg' => false,
-            //验证码字符大小
             'fontSize' => 25,
-            // 是否使用混淆曲线
             'useCurve' => false,
-            //是否添加杂点
             'useNoise' => true,
-            // 验证码字体 不设置则随机
             'fontttf' => '',
-            //背景颜色
             'bg' => [243, 251, 254],
-            // 验证码图片高度
             'imageH' => 0,
-            // 验证码图片宽度
             'imageW' => 0,
-
-            // 添加额外的验证码设置
-            // verify => [
-            //     'length'=>4,
-            //    ...
-            //],
         ];
         $con = new \think\Config();
         $con->set($config, "captcha");
@@ -176,38 +119,19 @@ class text extends create
     {
 
         $config = [
-            //验证码位数
             'length' => 4,
-            // 验证码字符集合
             'codeSet' => '0123456789',
-            // 验证码过期时间
             'expire' => 1800,
-            // 是否使用中文验证码
             'useZh' => false,
-            // 是否使用算术验证码
             'math' => true,
-            // 是否使用背景图
             'useImgBg' => false,
-            //验证码字符大小
             'fontSize' => 25,
-            // 是否使用混淆曲线
             'useCurve' => false,
-            //是否添加杂点
             'useNoise' => true,
-            // 验证码字体 不设置则随机
             'fontttf' => '',
-            //背景颜色
             'bg' => [243, 251, 254],
-            // 验证码图片高度
             'imageH' => 0,
-            // 验证码图片宽度
             'imageW' => 0,
-
-            // 添加额外的验证码设置
-            // verify => [
-            //     'length'=>4,
-            //    ...
-            //],
         ];
         $con = new \think\Config();
         $con->set($config, "captcha");
