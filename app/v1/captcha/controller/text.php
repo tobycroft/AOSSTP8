eda3ab0f3598659559f9d312b5de271384872892
火线兔 <silverhawk@foxmail.com>
2023-09-01 11:56:33 +0800

    TuuzMac

--- app/v1/captcha/controller/text.php
+++ app/v1/captcha/controller/text.php
@@ -50,6 +50,6 @@ class text extends create
         $sess = new Session($this->app);
         $capt = new Captcha($con, $sess);
         var_dump($sess);
-        return $capt->create();
+        $capt->create();
     }
 }
