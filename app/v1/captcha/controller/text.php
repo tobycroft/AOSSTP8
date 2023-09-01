f8b1a4603efcdd09780ac3963127a65f07c86967
火线兔 <silverhawk@foxmail.com>
2023-09-01 11:56:17 +0800

    TuuzMac

--- app/v1/captcha/controller/text.php
+++ app/v1/captcha/controller/text.php
@@ -49,6 +49,7 @@ class text extends create
 
         $sess = new Session($this->app);
         $capt = new Captcha($con, $sess);
+        var_dump($sess);
         return $capt->create();
     }
 }
