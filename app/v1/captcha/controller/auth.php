519edd7b5f404e21d5fe914234bf0f38021c1013
火线兔 <silverhawk@foxmail.com>
2023-09-01 13:25:57 +0800

    TuuzMac

---
+++ app/v1/captcha/controller/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\v1\captcha\controller;
+
+use app\v1\captcha\model\CaptchaModel;
+
+class auth extends text
+{
+    public function check()
+    {
+        $code = \Input::Post("code");
+        $capt = CaptchaModel::where("code", $code)->where("ident", $this->ident)->find();
+        if ($capt) {
+            $capt->delete();
+            \Ret::Success(0, null, "验证码正确");
+        } else {
+            \Ret::Fail(403, null, "验证码错误");
+        }
+    }
+
+}
