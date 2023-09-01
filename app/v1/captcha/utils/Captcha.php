4d251498604a5df24f8b2c9aad062c337f3c0b75
火线兔 <silverhawk@foxmail.com>
2023-09-01 11:55:45 +0800

    TuuzMac

--- app/v1/captcha/utils/Captcha.php
+++ app/v1/captcha/utils/Captcha.php
@@ -5,42 +5,5 @@ namespace app\v1\captcha\utils;
 class Captcha extends \think\captcha\Captcha
 {
 
-    private $session = null;
 
-    protected function generate(): array
-    {
-        $bag = '';
-
-        if ($this->math) {
-            $this->useZh = false;
-            $this->length = 5;
-
-            $x = random_int(10, 30);
-            $y = random_int(1, 9);
-            $bag = "{$x} + {$y} = ";
-            $key = $x + $y;
-            $key .= '';
-        } else {
-            if ($this->useZh) {
-                $characters = preg_split('/(?<!^)(?!$)/u', $this->zhSet);
-            } else {
-                $characters = str_split($this->codeSet);
-            }
-
-            for ($i = 0; $i < $this->length; $i++) {
-                $bag .= $characters[random_int(0, count($characters) - 1)];
-            }
-
-            $key = mb_strtolower($bag, 'UTF-8');
-        }
-
-        $hash = password_hash($key, PASSWORD_BCRYPT, ['cost' => 10]);
-
-        echo $hash . "-" . $bag;
-
-        return [
-            'value' => $bag,
-            'key' => $hash,
-        ];
-    }
 }
