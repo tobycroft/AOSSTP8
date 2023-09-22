d6b3c0c6146669beeb2e79fabc2349ed41e5a0a8
火线兔 <silverhawk@foxmail.com>
2023-09-22 14:22:42 +0800

    TuuzMac

--- app/v1/excel/controller/index.php
+++ app/v1/excel/controller/index.php
@@ -113,9 +113,7 @@ class index extends CommonController
         $colums = [];
         for ($i = 1; $i < count($datas); $i++) {
             $line = $datas[$i];
-            if (empty($line[0]) && strlen($line[0]) < 1) {
-                var_dump($line);
-                exit();
+            if (empty($line[0])) {
                 continue;
             }
             for ($s = 0; $s < $count_column; $s++) {
