6973492d1eed0eaedd89b79b3e6cc1c0155bf968
tuuz <silverhawk@foxmail.com>
2024-02-19 14:12:03 +0800

    TuuzPC

--- app/v1/rcon/action/ShowPlayerAction.php
+++ app/v1/rcon/action/ShowPlayerAction.php
@@ -10,10 +10,11 @@ class ShowPlayerAction
 
         $array = [];
         $key = explode(',', $lines[0]);
-
+        var_dump($key);
         foreach ($lines as $index => $value) {
             $temp = explode(',', $value);
             if ($index > 0) {
+                var_dump($temp);
                 $array[] = [
                     $key[0] => $temp[0],
                     $key[1] => $temp[1],
