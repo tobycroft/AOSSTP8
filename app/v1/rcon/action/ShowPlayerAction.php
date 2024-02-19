e536fed775bfb312629e075262eb0ba226c59c5e
tuuz <silverhawk@foxmail.com>
2024-02-19 14:14:26 +0800

    TuuzPC

--- app/v1/rcon/action/ShowPlayerAction.php
+++ app/v1/rcon/action/ShowPlayerAction.php
@@ -4,24 +4,24 @@ namespace app\v1\rcon\action;
 
 class ShowPlayerAction
 {
+
+    public array $arr = [];
+
     public function __construct($rcon_output)
     {
+//        $this->arr = [];
         $lines = explode("\n", $rcon_output);
-
-        $array = [];
         $key = explode(',', $lines[0]);
-        var_dump($key);
         foreach ($lines as $index => $value) {
             $temp = explode(',', $value);
             if ($index > 0) {
-                var_dump($temp);
-                $array[] = [
+                $this->arr[] = [
                     $key[0] => $temp[0],
                     $key[1] => $temp[1],
                     $key[2] => $temp[2],
                 ];
             }
         }
-        return $array;
+        return $this->arr;
     }
 }
