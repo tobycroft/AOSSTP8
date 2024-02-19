6803528dd5d131783ffd72b6c6717411dbec3073
tuuz <silverhawk@foxmail.com>
2024-02-19 12:29:21 +0800

    TuuzPC

--- app/v1/rcon/controller/index.php
+++ app/v1/rcon/controller/index.php
@@ -36,6 +36,8 @@ class index extends create
         $conn = new SourceQuery();
         $conn->Connect($this->rcon_info["ip"], $this->rcon_info["port"], 3, SourceQuery::SOURCE);
         $conn->SetRconPassword($this->rcon_info["password"]);
-        Ret::Success(0, trim($conn->Rcon('Ping')));
+        if (trim($conn->Rcon('Ping')) != "Pong") {
+            Ret::Fail(500, null, '连接服务器失败');
+        }
     }
 }
