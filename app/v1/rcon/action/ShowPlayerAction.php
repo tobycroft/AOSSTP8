a6c5caf060877d169c9027b095b8d9865b86c6b3
tuuz <silverhawk@foxmail.com>
2024-02-19 14:10:36 +0800

    TuuzPC

--- app/v1/rcon/action/ShowPlayerAction.php
+++ app/v1/rcon/action/ShowPlayerAction.php
@@ -8,17 +8,18 @@ class ShowPlayerAction
     {
         $lines = explode("\n", $rcon_output);
 
-        $array = array();
+        $array = [];
+        $key = explode(',', $lines[0]);
 
-        for ($i = 1; $i < count($lines); $i++) {
-            //使用explode函数按逗号分割每一行，得到一个临时的一维数组
-            $temp = explode(',', $lines[$i]);
-            //将临时数组的元素按照表头的顺序作为键值对，添加到二维数组中
-            $array[] = array(
-                'name' => $temp[0],
-                'playeruid' => $temp[1],
-                'steamid' => $temp[2]
-            );
+        foreach ($lines as $index => $value) {
+            $temp = explode(',', $value);
+            if ($index > 0) {
+                $array[] = [
+                    $key[0] => $temp[0],
+                    $key[1] => $temp[1],
+                    $key[2] => $temp[2],
+                ];
+            }
         }
         return $array;
     }
