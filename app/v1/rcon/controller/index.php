d39eb9a853c5108f4af6248d04335a4b8310999e
tuuz <silverhawk@foxmail.com>
2024-02-19 12:17:08 +0800

    TuuzPC

--- app/v1/rcon/controller/index.php
+++ app/v1/rcon/controller/index.php
@@ -32,6 +32,5 @@ class index extends create
     {
         $conn = new SourceQuery();
         $conn->Connect($this->rcon_info["ip"], $this->rcon_info["port"], 3, SourceQuery::SOURCE);
-        Ret::Success(0, [$conn->GetInfo(), $conn->GetPlayers(), $conn->GetRules()]);
     }
 }
