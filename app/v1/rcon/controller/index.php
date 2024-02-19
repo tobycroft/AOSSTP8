929341da22b1465116dfc40080e45ec6a87b43f7
tuuz <silverhawk@foxmail.com>
2024-02-19 12:19:32 +0800

    TuuzPC

--- app/v1/rcon/controller/index.php
+++ app/v1/rcon/controller/index.php
@@ -32,6 +32,7 @@ class index extends create
     {
         $conn = new SourceQuery();
         $conn->Connect($this->rcon_info["ip"], $this->rcon_info["port"], 3, SourceQuery::SOURCE);
+        $conn->SetRconPassword($this->rcon_info["password"]);
         Ret::Success(0, [$conn->Ping()]);
     }
 }
