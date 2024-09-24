4736dd9806126764215bd7697e3367bd85799701
tuuz <silverhawk@foxmail.com>
2024-09-25 01:11:47 +0800

    TuuzPC

--- app/v1/cert/controller/bt.php
+++ app/v1/cert/controller/bt.php
@@ -5,34 +5,35 @@ namespace app\v1\cert\controller;
 use app\v1\cert\model\CertModel;
 use BaseController\CommonController;
 use Input;
+use yixinba\Bt\Site;
 
 class bt extends CommonController
 {
+    public $cert;
 
     public function initialize()
     {
         parent::initialize();
         $tag = Input::Get('tag');
         $key = Input::Get('key');
-        $certs = CertModel::where('tag', $tag)->where('key', $key)->find();
-        if (!$certs) {
+        $this->cert = CertModel::where('tag', $tag)->where('key', $key)->find();
+        if (!$this->cert) {
             \Ret::Fail("404", null, "未找到证书项目");
         }
     }
 
     public function test()
     {
+        $this->site = new Site($this->cert['api'], $this->cert['key'], './');
         return json_encode($this->site->getList(), 320);
     }
 
     public function getlist()
     {
-        \Ret::Success(0, $this->site->getList()['data']);
     }
 
     public function pullssl()
     {
-        $this->site->pullSSL();
     }
 
     public function autossl()
