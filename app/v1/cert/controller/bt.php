c7a5410e4674ffd0bdd0cefef20f47c15bda9665
tuuz <silverhawk@foxmail.com>
2024-09-24 23:00:13 +0800

    TuuzPC

--- app/v1/cert/controller/bt.php
+++ app/v1/cert/controller/bt.php
@@ -5,12 +5,11 @@ namespace app\v1\cert\controller;
 use app\v1\cert\model\CertModel;
 use BaseController\CommonController;
 use Input;
-use yixinba\Bt\Base;
 use yixinba\Bt\Site;
 
 class bt extends CommonController
 {
-    public Base $bt_base;
+    public Site $site;
 
     public function initialize()
     {
@@ -20,8 +19,7 @@ class bt extends CommonController
         if (!$certs) {
             \Ret::Fail("404", null, "未找到证书项目");
         }
-        $bt_base = new Site($certs["url"], $certs["key"], './');
-        \Ret::Success(0, $bt_base->getList());
+        $this->site = new Site($certs["url"], $certs["key"], './');
     }
 
     public function test()
