5e68bf4560bc150a8cab700cf144e842dcef503c
tuuz <silverhawk@foxmail.com>
2024-09-24 23:16:38 +0800

    TuuzPC

--- app/v1/cert/controller/bt.php
+++ app/v1/cert/controller/bt.php
@@ -24,6 +24,6 @@ class bt extends CommonController
 
     public function test()
     {
-        \Ret::Success(0, $this->site->getSSL('www.tuuz.cn'));
+        return $this->site->getSSL('www.tuuz.cn');
     }
 }
