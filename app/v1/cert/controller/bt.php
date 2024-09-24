b4b03e1a6a21c4bed8edf2c02e53eb7d5d174a54
tuuz <silverhawk@foxmail.com>
2024-09-24 22:37:24 +0800

    TuuzPC

--- app/v1/cert/controller/bt.php
+++ app/v1/cert/controller/bt.php
@@ -5,10 +5,11 @@ namespace app\v1\cert\controller;
 use app\v1\cert\model\CertModel;
 use BaseController\CommonController;
 use Input;
+use yixinba\Bt\Base;
 
 class bt extends CommonController
 {
-    public $bt_base;
+    public Base $bt_base;
 
     public function initialize()
     {
@@ -18,12 +19,11 @@ class bt extends CommonController
         if (!$certs) {
             \Ret::Fail("404", null, "未找到证书项目");
         }
-//        $this->bt_base = new Base($panel, $key, $cookiePath)
-
+        $this->bt_base = new Base($certs["url"], $certs["key"], './');
     }
 
     public function test()
     {
-
+        $this->bt_base->getSystemTotal();
     }
 }
