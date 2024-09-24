64a607525b2f3719756a70df5958b719effa2a77
tuuz <silverhawk@foxmail.com>
2024-09-24 20:01:51 +0800

    TuuzPC

--- app/v1/cert/controller/index.php
+++ app/v1/cert/controller/index.php
@@ -2,10 +2,25 @@
 
 namespace app\v1\hook\controller;
 
+use app\v1\project\model\ProjectModel;
 use BaseController\CommonController;
 
-class ssh extends CommonController
+class index extends CommonController
 {
+
+    public function initialize()
+    {
+        set_time_limit(0);
+        parent::initialize();
+        if (!$this->token) {
+            $this->token = Input::Combi('token');
+        }
+        $this->proc = ProjectModel::api_find_token($this->token);
+        if (!$this->proc) {
+            Ret::Fail(401, null, '项目不可用');
+        }
+    }
+
     public function test()
     {
         $connection = ssh2_connect("10.0.0.182", 22);
