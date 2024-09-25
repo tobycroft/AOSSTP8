7be8ca49e2fc09aac20a42154895e918349b697a
tuuz <silverhawk@foxmail.com>
2024-09-25 22:14:26 +0800

    TuuzPC

--- app/v1/cert/controller/bt.php
+++ app/v1/cert/controller/bt.php
@@ -63,7 +63,12 @@ class bt extends CommonController
             \Ret::Fail(404, null, "项目中没有该站点，请先在自动更新库中添加本站点");
         }
         $this->site = new Site($site['api'], $site['key'], './');
-        \Ret::Success(0, $this->site->setSSL(1, $website, $ssl['crt'], $ssl['key']));
+        $ret = $this->site->setSSL(1, $website, $ssl['crt'], $ssl['key']);
+        if ($ret) {
+            \Ret::Success(0, $ret);
+        } else {
+            \Ret::Fail(500, $ret);
+        }
 
     }
 
