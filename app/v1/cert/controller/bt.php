20e005dfb44cdf1c94ab4537f794f2b192439b50
tuuz <silverhawk@foxmail.com>
2024-09-25 22:46:52 +0800

    TuuzPC

--- app/v1/cert/controller/bt.php
+++ app/v1/cert/controller/bt.php
@@ -52,14 +52,14 @@ class bt extends CommonController
     {
         $name = Input::Get('cert');
         $website = Input::Get('website');
+        $site = CertWebsiteModel::where('website', $website)->where('cert_url_tag', $name)->where('status', 1)->find();
+        if (!$site) {
+            \Ret::Fail(404, null, "项目中没有该站点，请先在自动更新库中添加本站点");
+        }
         try {
             $ssl = SslAction::updatessl($name);
         } catch (Exception $e) {
             \Ret::Fail('500', null, $e->getMessage());
-        }
-        $site = CertWebsiteModel::where('website', $website)->where('cert_url_tag', $name)->where('status', 1)->find();
-        if (!$site) {
-            \Ret::Fail(404, null, "项目中没有该站点，请先在自动更新库中添加本站点");
         }
         $bt_site = new Site($site['api'], $site['key'], './');
         $ret = $bt_site->setSSL(1, $website, $ssl['key'], $ssl['crt']);
