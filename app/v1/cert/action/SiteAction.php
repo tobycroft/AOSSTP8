60e6c81d303a0f809de9cbeae324183bc1813d7d
tuuz <silverhawk@foxmail.com>
2024-09-26 03:54:03 +0800

    TuuzPC

--- app/v1/cert/action/SiteAction.php
+++ app/v1/cert/action/SiteAction.php
@@ -37,6 +37,7 @@ class SiteAction
         $insertData = [];
         $certNames = CertUrlModel::column('cert');
         $siteNames = CertWebsiteModel::whereIn('cert_name', $certNames)->column('website');
+        \Ret::Success(0, $siteNames);
         foreach ($ret['data'] as $site) {
             if ($site['ssl'] !== -1) {
                 if (isset($site['ssl']['subject'])) {
