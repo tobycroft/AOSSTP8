86056e455a21469dc49f06e0d29459743a2f59a0
tuuz <silverhawk@foxmail.com>
2024-10-17 11:38:59 +0800

    TuuzPC

--- app/v1/cidr/controller/index.php
+++ app/v1/cidr/controller/index.php
@@ -29,6 +29,7 @@ class index extends CommonController
     {
         list($subnet, $mask) = explode('/', $cidr);
         $ip = ip2long($subnet);
+        echo $mask;
         $mask = 0xffffffff << (32 - $mask);
         $start = $ip & $mask;
         $end = $start | (~$mask & 0xffffffff);
