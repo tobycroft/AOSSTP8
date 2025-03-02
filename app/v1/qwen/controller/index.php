d1e58ae9cbbaf492606669234031bfb715a1e526
Tuuz <2539@tuuz.cn>
2025-03-03 03:40:30 +0800

    TuuzPC

--- app/v1/qwen/controller/index.php
+++ app/v1/qwen/controller/index.php
@@ -29,9 +29,9 @@ class index extends create
     public function aliyun()
     {
         $response = QwenClient::build($this->qwen['appkey'], 'https://dashscope.aliyuncs.com')
-            ->query('你好，今天是几月几日?')
-            ->query('今天是2025年1月2日', 'assistant')
-            ->query('你好，今天是几月几日?')
+            ->query('我说aaa你说bbb')
+//            ->query('今天是2025年1月2日', 'assistant')
+//            ->query('你好，今天是几月几日?')
             ->withModel('qwen2.5-1.5b-instruct')
 //            ->withModel('deepseek-r1-distill-llama-8b')
             ->run();
