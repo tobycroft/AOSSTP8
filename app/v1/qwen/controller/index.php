85e42fdf8bef8f2249f525eb9c87420b010cf2ac
Tuuz <2539@tuuz.cn>
2025-03-03 03:21:32 +0800

    TuuzPC

--- app/v1/qwen/controller/index.php
+++ app/v1/qwen/controller/index.php
@@ -27,7 +27,8 @@ class index extends create
 
     public function aliyun()
     {
-
+        echo $this->qwen['appkey'];
+        exit();
         $response = QwenClient::build($this->qwen['appkey'])
             ->query('Hello qwen, how are you today?')
             ->withModel('qwen2.5-1.5b-instruct')
