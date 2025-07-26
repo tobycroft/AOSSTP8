320ba72003de5f15bbb4e060e0d0295e35a99724
Tuuz <2539@tuuz.cn>
2025-07-27 06:48:41 +0800

    doudian

--- app/v2/doudian/controller/cookie.php
+++ app/v2/doudian/controller/cookie.php
@@ -15,7 +15,7 @@ class cookie extends index
         if ($cookie->isEmpty()) {
             Ret::Fail(404, null, 'Cookie not found'.$appid);
         } else {
-            Ret::Success(0, $cookie['cookie'],320, 'Cookie retrieved successfully');
+            Ret::Success(0, $cookie['cookie'], 'Cookie retrieved successfully');
         }
     }
 
