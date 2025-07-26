ad0f594ae1d743450dab8dab8e95efee74ff332c
Tuuz <2539@tuuz.cn>
2025-07-27 05:42:54 +0800

    doudian

--- app/v2/doudian/controller/cookie.php
+++ app/v2/doudian/controller/cookie.php
@@ -3,6 +3,7 @@
 namespace app\v2\doudian\controller;
 
 use app\v2\doudian\model\DoudianCookieModel;
+use Input;
 use Ret;
 
 class cookie extends index
@@ -15,6 +16,21 @@ class cookie extends index
             Ret::Fail(404, null, 'Cookie not found');
         } else {
             Ret::Success(0, $cookie['cookie'], 'Cookie retrieved successfully');
+        }
+    }
+
+    public function auto()
+    {
+        $appid = $this->project['appid'];
+        $cookie = DoudianCookieModel::where('appid', $appid)->find();
+        if ($cookie) {
+            $cookie->cookie = Input::Post('cookie');
+            $cookie->save();
+        } else {
+            $cookie = new DoudianCookieModel();
+            $cookie->appid = $appid;
+            $cookie->cookie = Input::Post('cookie');
+            $cookie->save();
         }
     }
 }
