8818fc2e224a2be02f011fa4bb318cdc3951fce4
Tuuz <2539@tuuz.cn>
2025-12-05 03:40:12 +0800

    更新验证码时间检查逻辑

--- app/v1/captcha/controller/auth.php
+++ app/v1/captcha/controller/auth.php
@@ -24,8 +24,11 @@ class auth extends text
     {
         $code = \Input::Post("code");
         $second = \Input::Post("second");
-        $capt = CaptchaModel::where("code", $code)->where("ident", $this->ident)->where("date", "<", time() - $second)->find();
+        $capt = CaptchaModel::where("code", $code)->where("ident", $this->ident)->find();
         if ($capt) {
+            if (strtotime($capt["date"]) - $second < time()) {
+                \Ret::Fail(403, null, "验证码已经过期啦");
+            }
             CaptchaIpModel::create(["ident" => $this->ident]);
             CaptchaModel::where("ident", $this->ident)->delete();
             \Ret::Success(0, null, "验证码正确");
