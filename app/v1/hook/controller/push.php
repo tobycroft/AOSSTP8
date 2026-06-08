f3bed52bb41790d81f1a7ebe72e529aed04ecd6a
火线兔 <tobycroft@users.noreply.github.com>
2026-06-08 12:50:53 +0800

    TuuzMac

--- app/v1/hook/controller/push.php
+++ app/v1/hook/controller/push.php
@@ -14,6 +14,7 @@ class push extends CommonController
     public function single()
     {
         $tag = Input::Get("tag");
+        $param=Input::Get("param",false);
         $data = HookModel::where("tag", $tag)->select();
         if ($data) {
             $rets = [];
@@ -26,6 +27,9 @@ class push extends CommonController
                             'access_key' => $datum['key'],
                             'param' => $datum['param'],
                         ];
+                        if(!empty($param)){
+                            $query['param'] = $param;
+                        }
                         $ret = Net::PostJson($path, $query);
                         $rets[$datum['remark']] = $ret;
                         if ($ret) {
