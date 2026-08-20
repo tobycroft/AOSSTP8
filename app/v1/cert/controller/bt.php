ece13f4866d1613a086bb100a9b7f4f0185a661c
tobycroft <astrohawk2010@gmail.com>
2026-08-20 23:13:41 +0800

    feat: autossl 失败时非网络错误自动置 status=0，同步站点列表异常保护

--- app/v1/cert/controller/bt.php
+++ app/v1/cert/controller/bt.php
@@ -107,7 +107,10 @@ class bt extends CommonController
         if ($cert['auto'] != 1) {
             \Ret::Fail(401, null, '本证书自动下发功能不可用');
         }
-        SiteAction::updateSiteListWhichHadSSL($this->cert['bt_api'], $this->cert['bt_key']);
+        try {
+            SiteAction::updateSiteListWhichHadSSL($this->cert['bt_api'], $this->cert['bt_key']);
+        } catch (Exception $e) {
+        }
         $ssl = SiteAction::updatessl($name);
 
         $sites = CertWebsiteModel::where('type', 'web')->where('cert_name', $name)->where('status', 1)->select();
