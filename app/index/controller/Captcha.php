afcd9a66c4a4831b8c9bf5cd6d3f097995c98ac5
tobycroft <24893075+tobycroft@users.noreply.github.com>
2026-09-18 17:45:13 +0800

    feat(captcha): 门户验证码改用 v1 动态 GIF 验证码

--- app/index/controller/Captcha.php
+++ app/index/controller/Captcha.php
@@ -2,7 +2,7 @@
 
 namespace app\index\controller;
 
-use app\admin\utils\Captcha as AdminCaptcha;
+use app\v1\captcha\utils\GifCaptcha;
 use BaseController\CommonController;
 
 class Captcha extends CommonController
@@ -16,10 +16,20 @@ class Captcha extends CommonController
             'useCurve' => true,
             'useNoise' => true,
             'bg' => [243, 251, 254],
-            'cookie_name' => 'user_captcha',
+            'frameDelay' => 100,
         ];
 
-        $capt = new AdminCaptcha($config);
-        return $capt->create();
+        $capt = new GifCaptcha($config);
+        $response = $capt->create();
+
+        // 验证码 hash 存入 cookie，供登录/注册校验
+        setcookie('user_captcha', $capt->hash, [
+            'expires' => time() + 300,
+            'path' => '/',
+            'httponly' => true,
+            'samesite' => 'Lax',
+        ]);
+
+        return $response;
     }
 }
